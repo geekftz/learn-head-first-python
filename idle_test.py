5acf54da3d4761224b4705d677ba38d474408f4a
fangxiang <fangxiang@immotors.com>
2025-05-23 15:10:21 +0800

    feat: init

---
+++ idle_test.py
@@ -0,0 +1,18 @@
+cast = ["Cleese", "Palin", "Jones" ,"Idle"]
+
+print(cast)
+
+
+print(len(cast))
+
+
+print(cast[1:2])
+
+cast.append("Gilliam")
+print(cast)
+
+cast.pop()
+print(cast)
+
+cast.extend(["Chapman", "Palin"])
+print(cast)
