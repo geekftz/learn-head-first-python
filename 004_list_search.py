79938fbc141bf3dc877e40c18c2f1c118ad26bde
fangxiang <fangxiang@immotors.com>
2025-05-26 10:04:22 +0800

    feat: add

---
+++ 004_list_search.py
@@ -0,0 +1,9 @@
+names = ["Michael", "Sarah", "John", "Anna", "David"]
+
+# isinstance --> python BIF(Built-in Functions) 内置函数
+flag = isinstance(names, list)
+print(flag)
+
+num_names = len(names)
+flag1 = isinstance(num_names, tuple)
+print(flag1)
