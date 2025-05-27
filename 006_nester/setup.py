b76f583253a571b54062e0d4bfa0a02492832fa2
fangxiang <fangxiang@immotors.com>
2025-05-27 13:55:43 +0800

    feat: add

--- 006_nester/setup.py
+++ 006_nester/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='nester',
-    version='1.0.0',
+    version='1.1.0',
     py_modules=['nester'],
     author='geekftz',
     author_email='472849306@qq.com',
