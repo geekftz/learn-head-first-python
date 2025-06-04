be22f768de0272cfe119bd6394baad29e19b41e2
fangxiang <fangxiang@immotors.com>
2025-06-04 16:55:14 +0800

    feat: add

--- code/chapter7/cgi-bin/generate_timing_data.py
+++ code/chapter7/cgi-bin/generate_timing_data.py
@@ -1,12 +1,12 @@
-#! /usr/local/bin/python3
+#! /usr/bin/python3
 
+import yate
+import athletemodel
 import cgi
 
 import cgitb
 cgitb.enable()
 
-import athletemodel
-import yate
 
 athletes = athletemodel.get_from_store()
 
@@ -14,11 +14,10 @@ form_data = cgi.FieldStorage()
 athlete_name = form_data['which_athlete'].value
 
 print(yate.start_response())
-print(yate.include_header("Coach Kelly's Timing Data"))    
+print(yate.include_header("Coach Kelly's Timing Data"))
 print(yate.header("Athlete: " + athlete_name + ", DOB: " +
-                      athletes[athlete_name].dob + "."))
+                  athletes[athlete_name].dob + "."))
 print(yate.para("The top times for this athlete are:"))
 print(yate.u_list(athletes[athlete_name].top3))
 print(yate.include_footer({"Home": "/index.html",
                            "Select another athlete": "generate_list.py"}))
-
